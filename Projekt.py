01d913e8828bc38b728db93dd86fdce0f34d9ae2
220493 <62022519+220493@users.noreply.github.com>
2020-04-26 19:39:20 +0200

    Add files via upload

--- Projekt.py
+++ Projekt.py
@@ -1,20 +1,3 @@
-"""volna_bunka = 0
-blokovana_cesta = 1
-i = hodnota v buňce testovacího bludiště"""
-
-testovaci_bludiste = [[0,1,0,1,1], [0,0,0,0,0], [1,0,1,0,1], [0,0,1,0,0], [1,0,0,1,0]]
-i = testovaci_bludiste[0] [0]
-syr = testovaci_bludiste [-1] [-1]
-
-ulozeni_reseni_1 = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
-j = ulozeni_reseni_1[0] [0]
-
-"""soucast_cesty = 1
-zbytek_bunek = 0
-j = hodnota v buňce řešení"""
-
-"""hledání cesty ven"""
-
 """x = souřadnice horizontálně"""
 """y = souřadnice vertikálně"""
 
@@ -22,36 +5,95 @@ x = 0
 y = 0
 """myš x = 0, y = 0"""
 
-def prvni_reseni(i,j,):
+"""volna_bunka = 0
+blokovana_cesta = 1
+i = hodnota v buňce testovacího bludiště"""
+
+testovaci_bludiste = [[0,1,0,1,1], [0,0,0,0,0], [1,0,1,0,1], [0,0,1,0,0], [1,0,0,1,0]]
+i = testovaci_bludiste[x] [y]
+testovaci_bludiste [-1] [-1] = 1000000
+syr = testovaci_bludiste [-1] [-1]
+
+"""syr = testovaci_bludiste [-1] [-1]"""
+
+ulozeni_reseni_1 = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
+j = ulozeni_reseni_1[x] [y]
+
+"""soucast_cesty = 1
+zbytek_bunek = 0
+j = hodnota v buňce řešení"""
+
+"""hledání cesty ven"""
+
+def prvni_reseni(i,j):
     while i == syr:
         pocetni_reseni_1 = 0
-        pocetni_reseni_1 += sum(j)
+        pocetni_reseni_1 += sum(range(j))
         return pocetni_reseni_1
-    else: kodove_reseni(i,j,x,y)
+    else: kodove_reseni(i,j,x,y,z,c)
 
-def kodove_reseni (i,j,x,y):
+z = 1
+c = z + 1
+def kodove_reseni (i, j, x, y, z, c):
     while i == 0:
         j += 1
         x += 1
-    else:
-        x -= 1
-        y -= 1
-        if i == 0:
-            j += 1
-        else:
-            y += 1
+        if i != 0:
             x -= 1
+            y -= 1
             if i == 0:
-                j += 1
+                return
             else:
-                x += 1
-                y += 1
+                 y += 1
+                 x -= 1
+                 if i == 0:
+                     return
+                 else:
+                     x += 1
+                     y += 1
+                     if i == 0:
+                         return
+    else:
+        y -= 1
+        if c != len(testovaci_bludiste [0]):
+            x += c
+            if i == 0:
+                return
+            else:
+                x -= c
+                y -= c
                 if i == 0:
-                    j +=1
+                    return
                 else:
-                    print("V bludišti neexistuje cesta k sýru.")
+                    y += c
+                    x -= c
+                    if i == 0:
+                        return
+                    else:
+                        x += c
+                        y += c
+                        if i == 0:
+                            return
+                        else:
+                            z += 1
+        else:
+            print("Bludiště nemá východ")
 
+print(prvni_reseni(i,j))
+"""def ostatni_reseni():
+    dalsi_reseni = -1
+    return dalsi_reseni
 
+def vysledne_reseni (pocetni_reseni_1, dalsi_reseni):
+    prvni_reseni(i,j)
+    ostatni_reseni()
+    hodnoty_reseni = []
+    hodnoty_reseni.append(int(pocetni_reseni_1))
+    hodnoty_reseni.append(int(dalsi_reseni))
+    hodnoty_reseni.sort()
+    return hodnoty_reseni"""
+
+"""print("Nejkratší cesta bludištěm:")"""
 
 
 
